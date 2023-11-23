05630bb46f1d9507475354fe942eb9f16a6378cf
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-11-23 11:13:33 +0100

    Update post.dart

--- lib/screens/datamodels/post.dart
+++ lib/screens/datamodels/post.dart
@@ -28,17 +28,21 @@ class Post {
     required this.body,
   });
 
-  factory Post.fromJson(Map<String, dynamic> json) => Post(
+    factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
         userId: json["userId"],
         id: json["id"],
         title: json["title"],
         body: json["body"],
       );
+  }
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() {
+    return {
         "userId": userId,
         "id": id,
         "title": title,
         "body": body,
       };
+  }
 }
